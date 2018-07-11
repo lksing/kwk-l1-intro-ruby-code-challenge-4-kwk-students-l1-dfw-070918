f64b2eac3186377ea4ef08b05ad5e221cac2c956
lksing <leiaxsing@gmail.com>
2018-07-11 20:51:57 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,32 @@
 #write out your code here
-
 def least_coins(cents)
-
 #Code your answer here!
-
+  if cents > 25
+    quarters = cents/25 
+    cents = cents % 25
+  else
+    quarters = 0
+  end
+  
+  if cents > 10
+    dimes = cents/10
+    cents = cents % 10
+  else 
+    dimes = 0
+  end
+    
+  if cents >= 5
+    nickels = cents/5
+    cents = cents%5
+  else 
+    nickels = 0
+  end
+  
+  if cents <= 5
+    pennies = cents
+  end
+    
+  puts optimal_coins = {"quarters" => quarters, "dimes" => dimes, "nickels" => nickels, "pennies" => pennies}
 end
+
+least_coins(100)
